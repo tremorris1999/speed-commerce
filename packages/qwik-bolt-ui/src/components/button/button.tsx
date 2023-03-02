26751d7c0f67568e7bc7e0f55730f9b34d550866
Trevor Morris <tremorris1999@gmail.com>
2023-03-02 08:16:21 +0000

    chore(ui): implements buttons refs: #16

--- packages/qwik-bolt-ui/src/components/button/button.tsx
+++ packages/qwik-bolt-ui/src/components/button/button.tsx
@@ -1,22 +1,49 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik'
+import {
+  QwikMouseEvent,
+  Slot,
+  component$,
+  useStylesScoped$
+} from '@builder.io/qwik'
 import styles from './button.scss?inline'
 export interface ButtonProps {
+  onClick?: (event: QwikMouseEvent<HTMLElement, MouseEvent>) => void
   theme?: 'light' | 'dark'
   variant?: 'primary' | 'secondary'
   text?: string
+  size?: 'xsmall' | 'small' | 'medium' | 'large'
+  rounded?: boolean
 }
 
 const determineClass = (props: ButtonProps): string => {
   const theme = props.theme || 'light'
   const variant = props.variant || 'primary'
-  return `${theme} ${variant}`
+  const size = props.size || 'medium'
+  const rounded = props.rounded ? 'rounded' : 'boxed'
+  return `${theme} ${variant} ${size} ${rounded}`
+}
+
+const mainContent = (props: ButtonProps) => {
+  if (props.text) {
+    return props.text
+  } else {
+    return <Slot />
+  }
 }
 
 export const Button = component$((props: ButtonProps) => {
   useStylesScoped$(styles)
   return (
     <>
-      <button class={determineClass(props)}>{props.text ?? 'Button'}</button>
+      <button
+        type="button"
+        class={determineClass(props)}
+        preventdefault:click
+        onClick$={props.onClick}
+      >
+        <Slot name="prepend" />
+        {mainContent(props)}
+        <Slot name="append" />
+      </button>
     </>
   )
 })
