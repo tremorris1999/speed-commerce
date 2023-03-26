565f4c62e1bae929ac30894350a96ba6da25f438
Trevor Morris <tremorris1999@gmail.com>
2023-03-26 11:02:07 +0000

    chore: add global dynamic imports for mdi icons

--- packages/sc-bolt-ui/src/components/icon/icon.tsx
+++ packages/sc-bolt-ui/src/components/icon/icon.tsx
@@ -1,11 +1,26 @@
 import { component$ } from '@builder.io/qwik'
-import { mdiAlertMinusOutline } from '@mdi/js'
 
-export const Icon = component$(() => {
+declare global {
+  // eslint-disable-next-line no-var
+  var icons: { [key: string]: string }
+}
+
+if (!global.icons) {
+  import('@mdi/js').then((mdi) => {
+    global.icons = mdi as unknown as { [key: string]: string }
+  })
+}
+
+const getIcon = (iconName: string) => {
+  const icon = global.icons[iconName]
+  return icon
+}
+
+export const Icon = component$((props: { iconName: string }) => {
   return (
     <>
       <svg style={'width:24px; height:24px'} viewBox="0 0 24 24">
-        <path d={mdiAlertMinusOutline} style={'fill: black;'} />
+        <path d={getIcon(props.iconName)} />
       </svg>
     </>
   )
