989cf9b2d9ae38cc35a9c784b103564f145b85de
Trevor Morris <tremorris1999@gmail.com>
2023-03-26 21:38:32 +0000

    fix(icon): properly index mdi namespace

--- packages/sc-bolt-ui/src/components/icon/icon.tsx
+++ packages/sc-bolt-ui/src/components/icon/icon.tsx
@@ -1,26 +1,10 @@
 import { component$ } from '@builder.io/qwik'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var icons: { [key: string]: string }
-}
-
-if (!global.icons) {
-  import('@mdi/js').then((mdi) => {
-    global.icons = mdi as unknown as { [key: string]: string }
-  })
-}
-
-const getIcon = (iconName: string) => {
-  const icon = global.icons[iconName]
-  return icon
-}
-
 export const Icon = component$((props: { iconName: string }) => {
   return (
     <>
       <svg style={'width:24px; height:24px'} viewBox="0 0 24 24">
-        <path d={getIcon(props.iconName)} />
+        <path d={global.icons[props.iconName]} />
       </svg>
     </>
   )
--- packages/sc-bolt-ui/src/index.ts
+++ packages/sc-bolt-ui/src/index.ts
@@ -1,2 +1,13 @@
+declare global {
+  // eslint-disable-next-line no-var
+  var icons: { [key: string]: string }
+}
+
+if (!global.icons) {
+  import('@mdi/js').then((mdi) => {
+    global.icons = mdi as Omit<typeof mdi, 'default'>
+  })
+}
+
 export { Button } from './components/button/button'
 export { Icon } from './components/icon/icon'
